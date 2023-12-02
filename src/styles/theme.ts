3649b5064e46786b228670b52b323ec033926b92
Joao Vinicius <joaovvs@gmail.com>
2023-12-02 17:40:52 -0300

    FIRST COMMIT

---
+++ src/styles/theme.ts
@@ -0,0 +1,24 @@
+export default{
+    COLORS: {
+        //PRODUCT COLORS
+        BLUE_DARK: "#1E6F9F",
+        BLUE: "#4EA8DE",
+        PURPLE_DARK: "#5360CE",
+        PURPLE: "#8284FA",
+
+        //BASE COLORS
+        GRAY_700: "#0D0D0D",
+        GRAY_600: "#1A1A1A",
+        GRAY_500: "#262626",
+        GRAY_400: "#333333",
+        GRAY_300: "#808080",
+        GRAY_200: "#D9D9D9",
+        GRAY_100: "#F2F2F2",
+
+
+        //FEEDBACK
+
+        DANGER: "#E25858"
+
+    }
+}
